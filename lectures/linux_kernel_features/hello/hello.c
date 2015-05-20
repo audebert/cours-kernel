baa0636688a45054368772d24c64f18640e1f006
Rémi Audebert <halfr@lse.epita.fr>
2015-05-20 17:38:50 +0200

    linux_kernel_features: add hello module

---
+++ lectures/linux_kernel_features/hello/hello.c
@@ -0,0 +1,22 @@
+/* hello.c */
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/printk.h>
+
+static int __init hello_init(void)
+{
+		pr_info("Hello, world!\n");
+			return 0;
+}
+
+static void __exit hello_exit(void)
+{
+		pr_info("Goodbye.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Hello module");
+MODULE_AUTHOR("Rémi Audebert");
