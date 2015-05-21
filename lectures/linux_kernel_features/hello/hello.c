f85ab0cbb40ef39ad044e30f84f02b6e988df439
Rémi Audebert <halfr@lse.epita.fr>
2015-05-21 18:45:47 +0200

    linux: fix hello.c

--- lectures/linux_kernel_features/hello/hello.c
+++ lectures/linux_kernel_features/hello/hello.c
@@ -3,16 +3,23 @@
 #include <linux/module.h>
 #include <linux/printk.h>
 
+
 static int __init hello_init(void)
 {
-		pr_info("Hello, world!\n");
-			return 0;
+	pr_info("Hello, world!\n");
+	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-		pr_info("Goodbye.\n");
+	pr_info("Goodbye.\n");
 }
+
+void hello_hello(void)
+{
+	pr_debug("hello! from module hello\n");
+}
+EXPORT_SYMBOL(hello_hello);
 
 module_init(hello_init);
 module_exit(hello_exit);
